57b03cbe70321e05883b38a343ce505fd2e0f134
Kalrav Mathur <km257@snu.edu.in>
2025-01-22 23:19:05 +0530

    Update oledDisplay.h

--- oledDisplay.h
+++ oledDisplay.h
@@ -4,7 +4,6 @@
 #include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
-
 // OLED display setup
 #define i2c_Address 0x3c
 #define SCREEN_WIDTH 128
@@ -14,7 +13,10 @@
 #define sdaPin 22
 
 extern Adafruit_SH1106G display;
-void displayTime(int, int, int);
-void displayDate(int, int, int);
-void displayDay(String);
+void displayHome(int, int, int, int, int, String);
+void displayStopWatch(int);
+void displayTimer(int);
+void displayWeather(int, String);
+void loadAnimation();
+
 #endif
