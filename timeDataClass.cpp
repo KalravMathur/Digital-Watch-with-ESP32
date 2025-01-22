a2c67fc0c3611fa9e5c177dcaa94cd666fd6c880
Kalrav Mathur <km257@snu.edu.in>
2025-01-22 23:15:40 +0530

    Update timeDataClass.cpp

--- timeDataClass.cpp
+++ timeDataClass.cpp
@@ -4,53 +4,133 @@
 #include <TimeLib.h>
 #include <WiFiUdp.h>
 #include "timeDataClass.h"
+#include "interrupts.h"
+#include "oledDisplay.h"
 
 // NTPClient setup
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", 19800, 60000); // Time offset (3600 for GMT+1), update interval
 
-void timeDateApi::updateTimeInfo()
+#define apiInterval 10000 // internval in millis (10 sec)
+unsigned int lastFetch = -apiInterval;
+
+void timeDataApi::updateTimeInfo()
 {
-    timeClient.update();
-    unsigned long epochTime = timeClient.getEpochTime();
+    unsigned int currentTime = millis();
+    if (currentTime - lastFetch > apiInterval)
+    {
+        timeClient.update();
+        lastFetch = currentTime;
+        unsigned long epochTime = timeClient.getEpochTime();
 
-    t1.hours = hourFormat12(epochTime);
-    t1.mins = minute(epochTime);
-    t1.secs = second(epochTime);
-    d1.date = day(epochTime);
-    d1.day = dayToWord(weekday(epochTime));
-    d1.month = month(epochTime);
-    d1.year = year(epochTime) % 2000;
+        t1.hours = hourFormat12(epochTime);
+        t1.mins = minute(epochTime);
+        d1.date = day(epochTime);
+        d1.day = dayToWord(weekday(epochTime));
+        d1.month = month(epochTime);
+        d1.year = year(epochTime) % 2000;
+    }
 }
-
-String timeDateApi::dayToWord(int n)
+String timeDataApi::dayToWord(int n)
 {
     String dayW;
     switch (n)
     {
     case 1:
-        dayW = "Sunday";
+        dayW = "Sun";
         return (dayW);
     case 2:
-        dayW = "Monday";
+        dayW = "Mon";
         return (dayW);
     case 3:
-        dayW = "Tuesday";
+        dayW = "Tue";
         return (dayW);
     case 4:
-        dayW = "Wednesday";
+        dayW = "Wed";
         return (dayW);
     case 5:
-        dayW = "Thursday";
+        dayW = "Thu";
         return (dayW);
     case 6:
-        dayW = "Friday";
+        dayW = "Fri";
         return (dayW);
     case 7:
-        dayW = "Saturday";
+        dayW = "Sat";
         return (dayW);
     default:
         dayW = "invalid";
         return (dayW);
     }
+}
+
+#define start b1.isPressed
+#define increment b2.isPressed
+#define isBack b3.isPressed
+#define MAX_INT 60
+int sec = 0;
+
+void timeDataApi::timerInterface()
+{
+    while (!isBack)
+    {
+        displayTimer(sec);
+        if (increment)
+        {
+            increment = false;
+            Serial.println("increment");
+            sec = sec + 10;
+        }
+
+        else if (start)
+        {
+            Serial.println("start");
+            start = false;
+
+            for (int i = sec; i >= 0; i--)
+            {
+                if (isBack)
+                {
+                    sec = 0;
+                    Serial.println("back to home");
+                    isBack = false;
+                    break;
+                }
+                displayTimer(i);
+                delay(1000);
+            }
+            sec = 0;
+        }
+        delay(200);
+    }
+    sec = 0;
+    isBack = false;
+    return;
+}
+
+void timeDataApi::stopWatchInterface()
+{
+    while (!isBack)
+    {
+        displayStopWatch(0);
+        if (start)
+        {
+            Serial.println("start");
+            start = false;
+
+            for (int i = 0; i <= MAX_INT; i++)
+            {
+                if (isBack)
+                {
+                    Serial.println("back to home");
+                    isBack = false;
+                    break;
+                }
+                displayStopWatch(i);
+                delay(1000);
+            }
+        }
+        delay(200);
+    }
+    isBack = false;
+    return;
 }
